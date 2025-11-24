99a24ce6d268c50af1671a99a1532fc024525f2f
emi-gc <230948780+emi-gc@users.noreply.github.com>
2025-11-24 12:09:39 -0600

    translated variables, vectors and comments to english

--- v1.cpp
+++ v1.cpp
@@ -5,26 +5,26 @@ using namespace std;
 int main() {
     
     int n;
-    cout << "Cuantos numeros tiene la frase codificada?: ";
-    cin >> n; // para saber de que tamano crear el vector y no desperdiciar / que falte memoria
+    cout << "How many numbers does the encoded phrase have?: ";
+    cin >> n; // to know the size to create the vector and not waste / lack memory
     
-    vector<int> codigo(n); // se crea vector tipo int para el texto codificado del tamano n
-    vector<char> resultado(n); // se crea vector tipo char para el texto decodificado de tamano n
+    vector<int> code(n); // int type vector for the encoded text of size n
+    vector<char> decoded(n); // char type vector for the decoded text of size n
     
-    cout << "Ingresa los " << n << " numeros (ASCII x2): " << endl; // loop que agrega los numeros ingresados al vector "codigo"
+    cout << "Enter the " << n << " numbers (ASCII x2): " << endl; // loop that adds entered numbers to the "code" vector
     for (int i = 0; i < n; i++) {
-        cout << "Ingresa el numero " << i+1 << ": ";
-        cin >> codigo[i];
+        cout << "Enter number " << i+1 << ": ";
+        cin >> code[i];
         cout << endl;
     }
     
-    for (int i = 0; i < n; i++) { // loop que decodifica numeros a ascii y divide entre dos, agregandolos al vector "resultado"
-        resultado[i] = char(codigo[i] / 2);
+    for (int i = 0; i < n; i++) { // loop that decodes numbers to ASCII and divides by two, adding them to the "decoded" vector
+        decoded[i] = char(code[i] / 2);
     }
     
-    cout << endl << "Frase decodificada: " << endl; // loop que muestra el vector resultado completo (frase decodificada)
-    for (int i = 0; i < resultado.size(); i++) {
-        cout << resultado[i];
+    cout << endl << "Decoded phrase: " << endl; // loop that displays the full "decoded" vector (decoded phrase)
+    for (int i = 0; i < decoded.size(); i++) {
+        cout << decoded[i];
     }    
     cout << endl;
 }
